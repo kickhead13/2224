b78b90d671959a3c1ce9641d082699a1e0746552
AlexDuna <daniel.alex17@yahoo.com>
2024-04-05 18:52:17 +0300

    Commit 3: movement fara ice effect
    Movement complet, follow the mouse + key movement control fara ice effect

--- Area2D.gd
+++ Area2D.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
-@export var speed = 700
+@export var speed = 200
 var velocity = Vector2.ZERO
 
+var mouse_position = null
 var screen_size #dimensiunea jocului
 
 func _ready():
@@ -12,7 +13,12 @@ func _ready():
 #we normalise the velocity so that the movement on the diagonal axis is not faster than horizontal or vertical axis
 #check if the player is moving and update the AnimatedSprite2D animation
 #$ is the same as get_node("AnimatedSprite2D").play()
-func _process(delta):
+func _physics_process(delta):
+	
+	velocity = Vector2(0,0)
+	
+	mouse_position = get_global_mouse_position()
+	look_at(mouse_position)
 	if Input.is_action_pressed("move_right") || Input.is_action_pressed("move_right2"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left") || Input.is_action_pressed("move_left2"):
@@ -21,13 +27,9 @@ func _process(delta):
 		velocity.y -= 1
 	if Input.is_action_pressed("move_down") || Input.is_action_pressed("move_down2"):
 		velocity.y += 1
-	
-	"""if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()		
-	else:
-		$AnimatedSprite2D.stop()"""
 		
+	velocity = velocity.normalized() * speed
 	#update the position of the player, and clamp it so that it can not leave the screen
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO,screen_size)
+	
