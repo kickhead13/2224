7aa090c187425d9f065904b6af6451767576b6e8
AlexDuna <daniel.alex17@yahoo.com>
2024-04-07 03:55:52 +0300

    Added Sounds
    Shoot,Background sound, Question Popup sound

--- src/main_menu.gd
+++ src/main_menu.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 var Info_Button = preload("res://src/info_button.tscn")
 var Start_Button = preload("res://src/start_button.tscn")
+var main_theme_audio = preload("res://resources/sounds/main_theme.wav")
+
+var main_theme = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,6 +15,11 @@ func _ready():
 	var start = Start_Button.instantiate()
 	start.position = Vector2(0, 120)
 	add_child(start)
+	
+	main_theme = AudioStreamPlayer.new()
+	main_theme.stream = main_theme_audio
+	main_theme.autoplay = true
+	add_child(main_theme)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- src/terminal.gd
+++ src/terminal.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
+var main_theme_audio = preload("res://resources/sounds/main_theme.wav")
+var main_theme = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play()
-
+	main_theme = AudioStreamPlayer.new()
+	main_theme.stream = main_theme_audio
+	main_theme.autoplay = true
+	add_child(main_theme)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
