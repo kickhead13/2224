3a5b29992021cf9b2fad23014faa1653bb2f5779
kickhead13 <alex_cioltea93@yahoo.com>
2024-04-08 12:21:17 +0300

    picture

--- src/node_2d.gd
+++ src/node_2d.gd
@@ -14,9 +14,9 @@ var question_effect_audio = preload("res://resources/sounds/Emergency Sound.wav"
 
 
 var Terminal = preload("res://src/terminal.tscn")
-var mob_spawn_rate = 400
+var mob_spawn_rate = 200
 var mob_drop_off = 30
-var popup_spawn_rate = 2000
+var popup_spawn_rate = 1000
 var popup_drop_off = 100
 
 var Bullet = BasicBullet
